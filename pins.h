29767882b0b60780b16f45cae545d476aac75d01
talalnas1r <mtn.talal77@gmail.com>
2025-08-28 20:38:45 +1000

    cleaning up

---
+++ pins.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// === Pin Definitions ===
+const int motorPin = 9;
+
+// Sensor 1
+const int trigPin = 5;
+const int echoPin = 6;
+
+// Sensor 2
+const int trigPin2 = 11;
+const int echoPin2 = 10;
+
+// Car Traffic Light LEDs
+const int carRed = 7;
+const int carYellow = 4;
+const int carGreen = 3;
+
+// Boat Traffic Light LEDs
+const int boatRed = 2;
+const int boatGreen = 8;
