ed603ccb68a31d7c7eb5d3c6b10742da0a0c1a01
talalnas1r <mtn.talal77@gmail.com>
2025-08-28 21:49:17 +1000

    saving Sensor work

---
+++ Sensors.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include "Sensors.h"
+
+long getDistance(int trig, int echo) {
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  long duration = pulseIn(echo, HIGH);
+  return duration * 0.034 / 2;
+}
---
+++ Sensors.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <Arduino.h>
+#include "pins.h"
+
+long getDistance(int trig, int echoPin);
